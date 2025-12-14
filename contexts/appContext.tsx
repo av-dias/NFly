4b9a464a3c394bf2602c52f2266ebf047cff902b
av-dias <al.vrdias@gmail.com>
2025-12-14 20:08:06 +0000

    [Context] User load and update logic

--- contexts/appContext.tsx
+++ contexts/appContext.tsx
@@ -1,4 +1,6 @@
+import { Hangar } from "@/model/hangarType";
 import { Player } from "@/model/playerType";
+import { fetchWithTimeout } from "@/service/serviceUtils";
 import { useFocusEffect } from "@react-navigation/native";
 import React, { createContext, useCallback, useState } from "react";
 
@@ -6,31 +8,81 @@ interface AppContext {
   serverConfig: [string, React.Dispatch<React.SetStateAction<string>>]; // Dev test only
   server: [boolean, React.Dispatch<React.SetStateAction<boolean>>]; // Dev test only
   player: [Player, React.Dispatch<React.SetStateAction<Player>>]; // Dev test only
+  hangar: [Hangar, React.Dispatch<React.SetStateAction<Hangar>>]; // Dev test only
+  players: [Player[], React.Dispatch<React.SetStateAction<Player[]>>]; // Dev test only
+  setRefreshUser: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const AppContext = createContext<AppContext>({
   serverConfig: ["", () => {}],
   server: [false, () => {}],
   player: [{} as Player, () => {}],
+  hangar: [{} as Hangar, () => {}],
+  players: [[], () => {}],
+  setRefreshUser: () => {},
 });
 
 const AppContextProvider = ({ children }: { children: any }) => {
   const [server, setServer] = useState<string>("");
   const [player, setPlayer] = useState<Player>({} as Player);
+  const [hangar, setHangar] = useState<Hangar>({} as Hangar);
+  const [players, setPlayers] = useState<Player[]>([]);
   const [isServerOnline, setIsServerOnline] = useState<boolean>(false);
+  const [refreshUser, setRefreshUser] = useState(false);
 
   const value: AppContext = {
     serverConfig: [server, setServer],
     server: [isServerOnline, setIsServerOnline],
     player: [player, setPlayer],
+    hangar: [hangar, setHangar],
+    players: [players, setPlayers],
+    setRefreshUser: setRefreshUser,
   };
 
   useFocusEffect(
     useCallback(() => {
-      async function checkAvailablePlayers() {}
+      async function fetchData() {
+        const playersJson = await fetchWithTimeout(
+          `http://${server}:8080/nf/players`
+        );
 
-      checkAvailablePlayers();
-    }, [value.server])
+        if (playersJson) {
+          const players = await playersJson?.json();
+          console.log("Reloading players data.");
+          setPlayers(players.data);
+          setIsServerOnline(true);
+        }
+      }
+
+      if (server) fetchData();
+    }, [server])
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      async function fetchData() {
+        const playerJson = await fetchWithTimeout(
+          `http://${server}:8080/nf/players/${player.name}`
+        );
+
+        if (playerJson) {
+          const playerData = (await playerJson?.json()).data;
+          console.log(`Reloading player ${player.name} data.`);
+          setPlayer(playerData);
+
+          const hangarJson = await fetchWithTimeout(
+            `http://${server}:8080/nf/hangar/player/${playerData.name}`
+          );
+
+          if (hangarJson) {
+            const hangarData = (await hangarJson?.json()).data;
+            console.log(hangarData)
+            setHangar(hangarData);
+          }
+        }
+      }
+      if (player.name) fetchData();
+    }, [refreshUser, player.name])
   );
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
