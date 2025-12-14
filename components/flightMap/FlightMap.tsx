c4bdbf81e6e6796c4cac6b754d1560aa444ac7f8
av-dias <al.vrdias@gmail.com>
2025-12-14 10:52:23 +0000

    [App] Flight Map Basic

--- components/flightMap/FlightMap.tsx
+++ components/flightMap/FlightMap.tsx
@@ -1,67 +1,182 @@
 import Colors from "@/constants/Colors";
 import { Airport } from "@/model/airportType";
-import { useEffect, useRef } from "react";
-import { StyleSheet } from "react-native";
-import MapView, { Marker, Polyline } from "react-native-maps";
-import { View } from "../Themed";
+import { Job } from "@/model/jobType";
+import { text } from "@/styling/commonStyle";
+import { loadHoursMinutes } from "@/utility/calendar";
+import { useFocusEffect } from "expo-router";
+import React, { useCallback, useState } from "react";
+import { Dimensions, Pressable, StyleSheet } from "react-native";
+import { Text, View } from "../Themed";
+import CustomPressable from "../customPressable";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+const calculateBounds = (jobs: Job[]) => {
+  if (jobs.length === 0) {
+    return { minLat: 0, maxLat: 0, minLon: 0, maxLon: 0 };
+  }
+
+  if (!jobs[0].airport) return { minLat: 0, maxLat: 0, minLon: 0, maxLon: 0 };
+  let minLat = jobs[0].airport.latitude;
+  let maxLat = jobs[0].airport.latitude;
+  let minLon = jobs[0].airport.longitude;
+  let maxLon = jobs[0].airport.longitude;
+
+  jobs.forEach((job) => {
+    if (job.airport) {
+      minLat = Math.min(minLat, job.airport.latitude);
+      maxLat = Math.max(maxLat, job.airport.latitude);
+      minLon = Math.min(minLon, job.airport.longitude);
+      maxLon = Math.max(maxLon, job.airport.longitude);
+    }
+  });
+
+  return { minLat, maxLat, minLon, maxLon };
+};
+
+const toPercentageCoordinates = (
+  lat: number,
+  lon: number,
+  minLat: number,
+  maxLat: number,
+  minLon: number,
+  maxLon: number
+) => {
+  // Convert longitude to percentage (left)
+  const leftPercentage = ((lon - minLon) / (maxLon - minLon)) * 80;
+
+  // Convert latitude to percentage (top)
+  // Note: Latitude is inverted because higher lat = lower on the screen
+  const topPercentage = ((maxLat - lat) / (maxLat - minLat)) * 80;
+
+  return { leftPercentage, topPercentage };
+};
+
+const toPixelCoordinates = (
+  leftPercentage: number,
+  topPercentage: number,
+  screenWidth: number,
+  screenHeight: number
+) => {
+  const x = (leftPercentage / 100) * screenWidth;
+  const y = (topPercentage / 100) * screenHeight;
+
+  return { x, y };
+};
 
 const FlightMap = ({
   location,
-  airports,
+  jobs: jobs,
 }: {
   location: Airport;
-  airports: Airport[];
+  jobs: Job[];
 }) => {
-  if (!location || !airports) return;
+  const [disabledList, setDisabledList] = useState<number[]>([]);
+  const [coordinates, setCoordenates] = useState<
+    ((Job & { leftPercentage: number; topPercentage: number }) | null)[]
+  >([]);
+  if (!location || !jobs || jobs.length === 0) return null;
 
-  const mapRef = useRef<MapView>(null);
-  const coordinates = airports.map((airport) => ({
-    latitude: airport.latitude,
-    longitude: airport.longitude,
-  }));
+  useFocusEffect(
+    useCallback(() => {
+      // Calculate bounds
+      const jobList = jobs.filter((a, index) => !disabledList.includes(index));
+      const { minLat, maxLat, minLon, maxLon } = calculateBounds(jobList);
 
-  useEffect(() => {
-    if (mapRef.current && coordinates.length > 0) {
-      // Automatically zoom and center the map to fit all coordinates
-      (mapRef.current as MapView).fitToCoordinates(coordinates, {
-        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }, // Padding around the markers
-        animated: true, // Smooth zoom animation
+      // Convert coordinates to percentages
+      const coordinatesList = jobList.map((job) => {
+        const { leftPercentage, topPercentage } = toPercentageCoordinates(
+          job?.airport?.latitude ?? 0,
+          job?.airport?.longitude ?? 0,
+          minLat,
+          maxLat,
+          minLon,
+          maxLon
+        );
+
+        const { x, y } = toPixelCoordinates(
+          leftPercentage,
+          topPercentage,
+          screenWidth,
+          screenHeight
+        );
+
+        return { ...job, leftPercentage, topPercentage, x, y };
       });
-    }
-  }, []);
+
+      setCoordenates(coordinatesList);
+    }, [disabledList])
+  );
 
   return (
     <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        style={styles.map}
-        initialRegion={{
-          latitude: location.latitude,
-          longitude: location.longitude,
-          latitudeDelta: 5,
-          longitudeDelta: 5,
-        }}
-        zoomControlEnabled={true}
-      >
-        {/* Draw lines between adjacent points */}
-        <Polyline
-          coordinates={coordinates}
-          strokeColor={Colors.dark.accent} // Red line
-          strokeWidth={3}
-        />
-
-        {/* Render markers for each point */}
-        {coordinates.map((point, index) => (
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: point.latitude,
-              longitude: point.longitude,
+      {/* Black background */}
+      <View style={styles.map}>
+        {disabledList.length > 0 && (
+          <View
+            style={{
+              padding: 10,
+              alignItems: "center",
             }}
-            title={`Point ${index + 1}`}
-          />
-        ))}
-      </MapView>
+          >
+            <CustomPressable
+              padding={5}
+              text={"Clear"}
+              color={Colors.dark.secundary}
+              onPress={() => setDisabledList([])}
+            />
+          </View>
+        )}
+        {/* Render markers */}
+        {coordinates.map(
+          (point, index) =>
+            point && (
+              <Pressable
+                key={index}
+                style={[
+                  styles.marker,
+                  {
+                    left: `${point.leftPercentage}%`,
+                    top: `${point.topPercentage}%`,
+                    width: 60,
+                    height: 60,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  },
+                ]}
+                onPress={() => setDisabledList((l) => [...l, index])}
+              >
+                <View
+                  style={{
+                    width: 10,
+                    aspectRatio: 1,
+                    borderRadius: 100,
+                    backgroundColor:
+                      index === 0 ? Colors.dark.accent : Colors.dark.warning,
+                  }}
+                />
+                <Text style={[text.mdTextSize, text.bold]}>
+                  {point.airport?.ident}
+                </Text>
+                {new Date(point.expiration).getDate() ==
+                  new Date().getDate() && (
+                  <>
+                    <Text style={text.mdTextSize}>
+                      {new Date(point.expiration).toLocaleTimeString()}
+                    </Text>
+                    <Text style={text.mdTextSize}>
+                      {loadHoursMinutes(
+                        (new Date(point.expiration).getTime() -
+                          new Date().getTime()) /
+                          (1000 * 60 * 60)
+                      )}
+                    </Text>
+                  </>
+                )}
+              </Pressable>
+            )
+        )}
+      </View>
     </View>
   );
 };
@@ -70,11 +185,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 10,
+    overflow: "hidden",
   },
   map: {
     width: "100%",
     height: "100%",
+    backgroundColor: Colors.dark.secundaryBackground,
+    position: "relative",
+  },
+  linesContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  line: {
+    position: "absolute",
+    height: 2,
+    backgroundColor: "white",
+  },
+  marker: {
+    position: "absolute",
+    width: 20,
+    height: 20,
     borderRadius: 10,
+    margin: 15,
   },
 });
 
